6ea1c8fa8e139222ea6ed32ded65a876d06d8ff3
hussain-ELshepeny <hussainacc1@gmail.com>
2026-01-30 14:01:28 +0200

    edit searchParams

--- lib/searchParams.ts
+++ lib/searchParams.ts
@@ -2,15 +2,17 @@ import {
   createSearchParamsCache,
   parseAsInteger,
   parseAsString,
+  parseAsArrayOf,
+  parseAsBoolean,
 } from "nuqs/server"
 
 export const searchParamsParsers = {
   q: parseAsString.withDefault(""),
-
-  category: parseAsString.withDefault(""),
-
-  sort: parseAsString.withDefault("newest"), // default sorting
-
+  category: parseAsArrayOf(parseAsString).withDefault([]),
+  minPrice: parseAsInteger.withDefault(0),
+  maxPrice: parseAsInteger.withDefault(2000),
+  inStock: parseAsBoolean.withDefault(false),
+  sort: parseAsString.withDefault("newest"),
   page: parseAsInteger.withDefault(1),
 }
 
